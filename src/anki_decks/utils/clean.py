838aa2aa11d7014a7fb238b68f3670edd15c46e2
dcronkite <dcronkite@gmail.com>
2023-01-22 11:35:28 -0800

    Add documentation

--- src/anki_decks/utils/clean.py
+++ src/anki_decks/utils/clean.py
@@ -2,4 +2,5 @@ import re
 
 
 def format_text(s, lower=True):
+    """Replace non-letter characters with underscore. Option to lowercase."""
     return re.sub(r'\W+', '_', s.lower() if lower else s)
--- src/anki_decks/utils/decks.py
+++ src/anki_decks/utils/decks.py
@@ -6,12 +6,14 @@ from anki_decks.utils.timestamp import get_timestamp
 
 
 def get_subcategory_deck(deck_id, category, label):
+    """Create deck subcategory of `category` called `label`; creates unique deck id."""
     return genanki.Deck(
         deck_id + abs(hash(label) % 1000000), f'{category}::{label}',
     )
 
 
-def build_name(name, include_timestamp=False):
+def build_filename(name, include_timestamp=False):
+    """Builds filename with or without timestamp; includes appropriate extensions."""
     if include_timestamp:
         return f'{name}_{get_timestamp()}.apkg'
     else:
@@ -19,4 +21,5 @@ def build_name(name, include_timestamp=False):
 
 
 def write_deck_to_file(deck, name, *, outdir=Path('.'), include_timestamp=False):
-    genanki.Package(deck).write_to_file(outdir / build_name(name, include_timestamp=include_timestamp))
+    """Write deck to file, including generating filename."""
+    genanki.Package(deck).write_to_file(outdir / build_filename(name, include_timestamp=include_timestamp))
